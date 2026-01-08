f67e40163be11b71350a0e17cb64b10b25a3c944
DhurvXD <149307419+DhurvXD@users.noreply.github.com>
2026-01-08 18:47:57 +0530

    Delete Front directory

--- Front/app/template.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import type React from "react"
-import { Providers } from "./providers"
-
-export default function Template({ children }: { children: React.ReactNode }) {
-  return <Providers>{children}</Providers>
-}
